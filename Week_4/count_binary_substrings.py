d257322ee6ed3d3f15789057021f1e3ff360964a
rahul singh <rahul.singh4800@gmail.com>
2021-04-24 01:04:56 +0530

    week 4 binary substrings count

---
+++ Week_4/count_binary_substrings.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-binary-substrings/
+
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        c = []
+        s+='$'
+        last = s[0]
+        temp = 0
+        for i in range(len(s)):
+            if last!=s[i]:
+                c.append(temp)
+                last = s[i]
+                temp = 1
+            else:
+                temp+=1
+        req = 0
+        for i in range(len(c)-1):
+            req+=min(c[i], c[i+1])
+        return req
+        
